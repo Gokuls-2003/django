87e127bc6069e6c9271454ae9d8c9f27c152531f
GOKUL SENTHIL <gokulsenthil899@gmail.com>
2024-02-03 22:30:33 +0530

    center html

--- myapp/views.py
+++ myapp/views.py
@@ -21,27 +21,40 @@ from django.http import HttpResponse
 #     text = """<h1 style="color: #F4CE14:">THIS IS LITTLE LEMON AGAIN!</h1>"""
 #     return HttpResponse(text)
 
-def home(request):
-    path = request.path
-    scheme = request.scheme
-    method = request.method
-    address = request.META['REMOTE_ADDR']
-    user_agent = request.META['HTTP_USER_AGENT']
-    path_info= request.path_info
+
+# creating request and response
+# def home(request):
+#     path = request.path
+#     scheme = request.scheme
+#     method = request.method
+#     address = request.META['REMOTE_ADDR']
+#     user_agent = request.META['HTTP_USER_AGENT']
+#     path_info= request.path_info
     
-    response = HttpResponse()
-    response.headers['AGE'] = 20
+#     response = HttpResponse()
+#     response.headers['AGE'] = 20
     
-    msg = f"""<br>
-    <br>path: {path}
-    <br>scheme: {scheme}
-    <br>address: {address}
-    <br>method: {method}
-    <br>user_agent: {user_agent}
-    <br>path_info: {path_info}
-    <br> Response header: {response.headers}
+#     msg = f"""<br>
+#     <br>path: {path}
+#     <br>scheme: {scheme}
+#     <br>address: {address}
+#     <br>method: {method}
+#     <br>user_agent: {user_agent}
+#     <br>path_info: {path_info}
+#     <br> Response header: {response.headers}
     
-    """
+#     """
    
-    return HttpResponse(msg, content_type='text/html', charset='utf-8')
+   # return HttpResponse(msg, content_type='text/html', charset='utf-8')
     # return HttpResponse(path, content_type='text/html', charset='utf-8')
+
+    #
+    
+def index(request):
+    path=request.path
+    method=request.method
+    content = '''<center><h2>Testing django request response object<h2>
+    <p>Resquest path : "{}</p>
+    <p>Resquest Method :{}</p></center>
+    '''.format(path,method)
+    return HttpResponse(content)
